946fa5d8f776c7c26c247ef57896d455f7caf8c3
toadfin <toadfin@proton.me>
2024-02-03 15:24:28 +0100

    debug env var fixed

--- environ.py
+++ environ.py
@@ -2,5 +2,5 @@ import os
 
 
 class Environ:
-    DEBUG = os.environ.get("DEBUG", True)
+    DEBUG = os.environ.get("DEBUG", "true") == "true"
     URL = os.environ.get("URL", "http://127.0.0.1:5000")
